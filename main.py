bcf8194f14c4ef556c59c7a0231e5414ceabfd99
gaarangoa <gaarangoa@gmail.com>
2018-02-14 15:36:53 -0500

    fix

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 from remove_cloroplasts import Cleaner
+
+reference = "./ggenes/gg_chloroplasts"
+paired_1 = 
+paired_2 = 
+output_path = 
+
+cleaner = Cleaner(  reference = reference, 
+                    paired_1 = ,
+                    paired_2 = ,
+                    output_path =                    
+)
+
--- remove_cloroplasts.py
+++ remove_cloroplasts.py
@@ -1,5 +1,5 @@
 from parse import parse_sam
-from Bio import SeqIO
+from Bio.SeqIO.QualityIO import FastqGeneralIterator
 
 class Cleaner():
 
@@ -16,19 +16,27 @@ class Cleaner():
         # assumes bowtie is installed in your machine
         cmd=" ".join([
             'bowtie2', 
-            '--fast-local --no-discordant -p '+self.cores,
-            ' --no-unal --no-hd --no-sq -x', self.reference, 
+            '--fast-local --no-discordant -p ', self.cores,
+            ' --no-unal --no-hd --no-sq -x ', self.reference, 
             '-1', self.paired_1, 
             '-2', self.paired_2, 
             '-S', self.bowtie_out_file'>>', self.bowtie_out_file+'.log', '2>&1'
         ])
         os.system(cmd)
-        parse_sam(fi = self.bowtie_out_file, fo = self.bes)
+        parse_sam(fi = self.bowtie_out_file, fox = self.best_hit_file)
+        listf = {i.split()[0]:True for i in open(self.best_hit_file)}
+        return listf
 
-    def retrive(self, fi='', listf=''):
-        cmd = "filter_fasta.py -f "+self.paired_1+" "+self.paired_1.replace(".fastq","")+".no-chl.fastq -s "+listf+" -n"
-        os.system(cmd)
-        cmd = "filter_fasta.py -f "+self.paired_2+" "+self.paired_2.replace(".fastq","")+".no-chl.fastq -s "+listf+" -n"
-        os.system(cmd)
-        
+    def retrive(self, fi='', listf={}):
+        for _id,seq,qual in FastqGeneralIterator(open(fi)):
+            header = _id.split(' ')[0] 
+            read = '@%s\n%s\n+\n%s' %(_id, seq, qual)
+            try:
+                listf[header]
+            except Exception as e:
+                fo.write(read+"\n")
+    
+    def process(self):
+        listf = self.call_bowtie()
+        retrieve(fi = self.paired_1, listf)
 
