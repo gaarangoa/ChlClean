8cda099d902f6f4eb122977807dc68e348adf215
gaarangoa <gaarangoa@gmail.com>
2018-02-14 14:20:34 -0500

    init

---
+++ parse.py
@@ -0,0 +1,29 @@
+
+def parse_sam(fi='', identity=80, coverage=25, fox=''):
+    BH={}
+    for line in open(fi):
+        values=line.replace("\n","").split("\t")
+        matchedbp=sum([int(x.strip('M')) for x in re.findall(r'(\d*M)', values[5])])
+        if  matchedbp<coverage: continue
+        try:
+            current_best_hit=int(line.split("NM:i:")[1].split()[0])
+            prev_best_hit=int(BH[values[0]].split("NM:i:")[1].split()[0])
+            if current_best_hit<prev_best_hit:
+                BH[values[0]]=line
+        except:
+            BH[values[0]]=line
+
+    fo = open(fox)
+    for i in BH:
+        line = BH[i]
+        values=line.replace("\n","").split("\t")
+        matchedbp=sum([int(x.strip('M')) for x in re.findall(r'(\d*M)', values[5])])
+        alignlen=float(len(values[9]))
+        read_identity = alignlen/matchedbp
+        if read_identity < identity: continue
+        if matchedbp < coverage: continue
+        fo.write(line)
+    
+    fo.close()
+        
+
---
+++ remove_cloroplasts.py
@@ -0,0 +1,34 @@
+from parse import parse_sam
+from Bio import SeqIO
+
+class Cleaner():
+
+    def __init__(self, reference='', paired_1='', paired_2='', output_path="", cores=8):
+        self.reference = reference
+        self.paired_1 = paired_1
+        self.paired_2 = paired_2
+        self.output_path = output_path
+        self.cores = cores
+        self.bowtie_out_file = self.output_path+'/bowtie.gg.sam'
+        self.best_hit_file = self.output_path+'/bowtie.gg.sam.bh'
+
+    def call_bowtie(self):
+        # assumes bowtie is installed in your machine
+        cmd=" ".join([
+            'bowtie2', 
+            '--fast-local --no-discordant -p '+self.cores,
+            ' --no-unal --no-hd --no-sq -x', self.reference, 
+            '-1', self.paired_1, 
+            '-2', self.paired_2, 
+            '-S', self.bowtie_out_file'>>', self.bowtie_out_file+'.log', '2>&1'
+        ])
+        os.system(cmd)
+        parse_sam(fi = self.bowtie_out_file, fo = self.bes)
+
+    def retrive(self, fi=''):
+        
+        for record in SeqIO.parse(open(fi), "fastq"):
+            
+            
+        
+
